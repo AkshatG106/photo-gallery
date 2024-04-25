fdac9eb0bddec2cbbd040112b30c32644f4f2f6b
vickyjayswal12 <vickyjayswal12@gmail.com>
2024-04-25 16:23:40 +0530

    third commit

--- src/component/registration/Register.js
+++ src/component/registration/Register.js
@@ -1,8 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import bg from "../../assests/images.jpg";
 import logo from "../../assests/NewLogo.png";
+import axios from "axios";
 
 const Register = () => {
+  const [userData, setUserData] = useState({
+    user_name: "",
+    email: "",
+    password: "",
+    confirmpassword: "",
+  });
+
+  const handleData = (e) => {
+    const { name, value } = e.target;
+    setUserData({
+      ...userData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://192.168.29.5:3002/register", userData)
+      .then((res) => console.log("data submitted successfully"))
+      .catch((err) => console.log("failed to submit",err.response.data));
+  };
+
   return (
     <div
       className="flex justify-center items-center h-screen bg-cover bg-center"
@@ -24,39 +48,50 @@ const Register = () => {
           </div>
         </div>
         <div className="rounded-b-lg p-4 w-[340px]">
-          <form className="m-3">
+          <form className="m-3" onSubmit={handleSubmit}>
             <div className="">
-              <div>UserName : </div>
+              <div>Username : </div>
               <input
+                name="user_name"
                 type="text"
                 className="border h-[40px] rounded-sm mt-2 pl-2 w-full"
+                onChange={handleData}
               />
             </div>
             <div className="mt-3">
               <div>Email : </div>
               <input
-                type="text"
+                type="email"
+                name="email"
                 className="border h-[40px] rounded-sm mt-2 pl-2 w-full"
+                onChange={handleData}
               />
             </div>
             <div className="mt-3">
               <div>Password : </div>
               <input
-                type="text"
+                type="password"
+                name="password"
                 className="border h-[40px] rounded-sm mt-2 pl-2 w-full"
+                onChange={handleData}
               />
             </div>
             <div className="mt-3">
               <div>Confirm Password : </div>
               <input
-                type="text"
+                type="password"
+                name="confirmpassword"
                 className="border h-[40px] rounded-sm mt-2 pl-2 w-full"
+                onChange={handleData}
               />
             </div>
             <div className="mt-3 ml-1 text-[12px] text-gray-400">
               Registration confirmation will be emailed to you
             </div>
-            <button className="bg-red-400 w-full rounded-md p-2 text-white mt-6">
+            <button
+              type="submit"
+              className="bg-red-400 w-full rounded-md p-2 text-white mt-6"
+            >
               Register
             </button>
           </form>
